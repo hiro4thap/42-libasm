b30d3bf56bd886b4f621160d3252eb42a628934d
Hiroaki Ono <hiono@c3r1s6.42tokyo.jp>
2025-12-20 18:32:54 +0900

    added test cases

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hiono <hiono@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 11:57:35 by hiono             #+#    #+#             */
-/*   Updated: 2025/12/20 12:21:46 by hiono            ###   ########.fr       */
+/*   Updated: 2025/12/20 18:31:02 by hiono            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,32 @@
 #include <printf.h>
 #include <string.h>
 
-int ft_strlen(const char *);
+size_t	ft_strlen(const char *src);
+char 	*ft_strcpy(char *dest, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+size_t	ft_write(int fd, const void *buf, size_t count);
+size_t	ft_read(int fd, void *buf, size_t count);
+char	*ft_strdup(const char *s);
 
-void main()
+int test_ft_strlen(size_t callback(const char *))
 {
-	const char * words = "Hello World!";
-	int res = ft_strlen(words);
-	int org_res = strlen(words);
-	printf("result: %d\n", res);
-	printf("result: %d\n", org_res);
+	const char *str01 = "Hello World!";
+	printf("src: \"%s\" -> return: %zu\n", str01, callback(str01));
+	const char *str02 = "this is a sample sentence for strlen";
+	printf("src: \"%s\" -> return: %zu\n", str02, callback(str02));
+	const char *str03 = "";
+	printf("src: \"%s\" -> return: %zu\n", str03, callback(str03));
+	//const char *str03 = NULL;
+	//printf("src: NULL -> return: %zu\n", callback(str03));
+	return 0;
+}
 
-	return;
+int main()
+{
+	printf("------ft_strlen------\n");
+	test_ft_strlen(ft_strlen);
+	printf("------strlen------\n");
+	test_ft_strlen(strlen);
+
+	return 0;
 }
